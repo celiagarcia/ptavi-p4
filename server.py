c78ea2f0603053ac83e9f96baa9754dbf06a3f96
celia garcia fernandez <cgarcia@iota10.aulas.gsyc.es>
2014-10-27 12:34:18 +0100

    realiza el apartado 8

--- server.py
+++ server.py
@@ -7,42 +7,64 @@ en UDP simple
 
 import SocketServer
 import sys
+import time
 
 clientes = {}
+
+def register2file(clientes):
+    #Vuelco el diccionario
+    fich = open('registered.txt', 'w')
+    fich.write('User\tIP\tExpires\r\n')
+    for cliente in clientes:
+        localhost = clientes[cliente][0][0]
+        caducidad = int(clientes[cliente][1])
+        cadena = cliente + '\t' + localhost + '\t'
+        cadena += time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(caducidad)) + '\r\n'
+        fich.write(cadena)
 
 class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
     """
     Echo server class
     """
-
     def handle(self):
     
+        #Actualizo el diccionario
+        for cliente in clientes.keys():
+            caducidad = int(clientes[cliente][1])
+            if caducidad <= time.time():
+                del clientes[cliente]
+
         # Escribe dirección y puerto del cliente (de tupla client_address)
         print self.client_address
         self.wfile.write("Hemos recibido tu peticion ")
-        
         while 1:
+         
             # Leyendo línea a línea lo que nos envía el cliente 
             line = self.rfile.read()
             if line != "":
-                print "El cliente nos manda " + line
-                #Trocea el mensaje
+                print "El cliente nos manda " + line       
+                        
+                #Guarda parámetros
                 troceo = line.split()
                 metodo = troceo[0]
-                direccion = troceo[1]
-                expires = int(troceo[4])        
-                #Guarda la dirección del cliente en el diccionario
+                direccion = troceo[1].split(':')[1]
+                expires = int(troceo[4])
+                caducidad = time.time() + expires 
+                      
+                #REGISTER
                 if metodo == 'REGISTER':
-                    clientes[direccion] = self.client_address
-                #Borra la direccion del cliente si expire=0
-                if expires == 0:
-                    del clientes[direccion]
-                #Responde al cliente
-                    self.wfile.write('SIP/1.0 200 OK\r\n\r\n')               
+                    self.wfile.write('SIP/1.0 200 OK\r\n\r\n')
+                    #Entra cliente
+                    clientes[direccion] = [self.client_address, caducidad]
+                    register2file(clientes)   
+                    #Se da de baja cliente
+                    if expires == 0:
+                        del clientes[direccion]
+                        register2file(clientes)
+                print clientes        
             else:
                 break
-        print clientes
-        
+
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
     PORT = int(sys.argv[1])
